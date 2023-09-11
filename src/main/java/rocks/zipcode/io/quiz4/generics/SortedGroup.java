8e6919969ca157909ef1ba2c8f4d06b6514bd023
Minsoo Jo <134647176+Minsoo5@users.noreply.github.com>
2023-09-11 14:44:29 -0400

    Stuck on sorted group

--- src/main/java/rocks/zipcode/io/quiz4/generics/SortedGroup.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/SortedGroup.java
@@ -1,24 +1,28 @@
 package rocks.zipcode.io.quiz4.generics;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * @author leon on 18/12/2018.
  */
 public class SortedGroup<_> extends Group<_> {
-    ArrayList<_> sorted = new ArrayList<>();
+    private Group<_> group = new Group<>();
+
     @Override
     public void insert(Object value) {
-        _ valueToInsert = (_) value;
-        sorted.add(valueToInsert);
+        group.insert(value);
     }
 
     @Override
     public void delete(Object value) {
-        sorted.remove(value);
+        group.delete(value);
     }
 
     public Integer indexOf(_ value) {
-        return null;
+        Iterator<_> sorted = group.iterator();
+        ArrayList<_> sortedList = new ArrayList<>();
+        sorted.forEachRemaining(sortedList::add);
+        return sortedList.indexOf(value);
     }
 }
